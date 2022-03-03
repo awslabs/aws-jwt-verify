433ca25608799973a1c37368d71175af336d6f65
Otto Kruse <ottokrus@amazon.nl>
2022-03-03 21:36:41 +0100

    Use rollup node resolve

--- tests/vite-app/vite.config.ts
+++ tests/vite-app/vite.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig } from "vite";
-import alias from "@rollup/plugin-alias";
+import resolve from "@rollup/plugin-node-resolve";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-      plugins: [
-        alias({
-          entries: [
-            {
-              find: "#node-web-compat",
-              replacement: "./node-web-compat-web.js",
-            },
-          ],
-        }),
-      ],
+      plugins: [resolve({ browser: true })],
     },
   },
 });
