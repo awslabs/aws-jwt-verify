8b4ccc077e6a0b292b9eb8c9eb2df7b3ade52664
Otto Kruse <ottokrus@amazon.nl>
2022-03-03 10:24:14 +0100

    Rollup fix with alias

--- tests/vite-app/vite.config.ts
+++ tests/vite-app/vite.config.ts
@@ -1,4 +1,20 @@
 import { defineConfig } from "vite";
+import alias from "@rollup/plugin-alias";
 
 // https://vitejs.dev/config/
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      plugins: [
+        alias({
+          entries: [
+            {
+              find: "#node-web-compat",
+              replacement: "./node-web-compat-web.js",
+            },
+          ],
+        }),
+      ],
+    },
+  },
+});
