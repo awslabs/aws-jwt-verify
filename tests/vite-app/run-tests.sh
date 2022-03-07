bdc16ba1da748ea5d8f059c466421ab6bf9b3c63
Kevin Hakanson <kevhak@amazon.com>
2022-03-07 07:35:34 -0600

    add vite preview server testing

--- tests/vite-app/run-tests.sh
+++ tests/vite-app/run-tests.sh
@@ -25,6 +25,30 @@ main() {
     if [ ! -z $TEST_FAILED ]; then
         return 1
     fi
+
+    echo "Building Vite app for preview ..."
+    npm run build
+    echo "Starting Vite preview server in background ..."
+    npm run preview &
+    VITE_PID=$!
+    echo "Vite preview server launched as PID ${VITE_PID}"
+    while ! nc -z localhost 4173; do
+        echo "Waiting for preview server to come on-line ..."
+        sleep 1
+    done
+    echo "Running cypress tests ..."
+    if ! npm run cypress:run:preview; then
+        echo "Cypress test failed :("
+        TEST_FAILED=true
+    fi
+    echo "Sending stop signal to Vite preview server (SIGINT) ..."
+    kill -s INT $VITE_PID
+    echo "Waiting for Vite preview server to actually stop ..."
+    wait
+    echo "Vite preview server stopped"
+    if [ ! -z $TEST_FAILED ]; then
+        return 1
+    fi
 }
 
 main
