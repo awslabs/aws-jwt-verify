777a501662e320757b59d2b63933b03e8426e866
Otto Kruse <ottokrus@amazon.nl>
2023-01-27 16:14:59 +0000

    Add name and version to dist type specific package.json (#112)

---
+++ dist-create-package.cjs
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const pkg = require("./package.json");
+const fs = require("fs");
+const path = require("path");
+
+const targetType = process.argv[2];
+
+if (!["cjs", "esm"].includes(targetType)) {
+  throw new Error("Specify cjs or esm");
+}
+
+const { name, version } = pkg;
+
+// eslint-disable-next-line security/detect-non-literal-fs-filename
+fs.writeFileSync(
+  path.join(__dirname, "dist", targetType, "package.json"),
+  JSON.stringify({
+    name,
+    version,
+    type: targetType === "cjs" ? "commonjs" : "module",
+    imports: {
+      "#node-web-compat": {
+        browser: "./node-web-compat-web.js",
+        default: "./node-web-compat-node.js",
+      },
+    },
+  })
+);
